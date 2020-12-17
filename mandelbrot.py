c068ff28336587704bc990eced0962f83451d78d
Rafael Rossi <rossirafael@yahoo.com>
2020-12-17 10:41:46 +0100

    Functional version.

---
+++ mandelbrot.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Iteractive plotting the Mandelbrot set
+
+How to
+======
+
+Select the domain to zoom in by dragging a rectangle in the plot.
+Double click to update the plot.
+
+Left click to zoom out.
+
+Up-key to increase max iters by factor of 2.
+
+Down-key to reduce max iters by a factor of 10.
+
+Press R to toggle rectangle selection.
+
+TODO
+====
+
+ - Optimise the algorithm. Very slow for max_iters of 1000.
+
+ - Fix the annoying rectangle selection that persists after zoom.
+
+@author: rarossi
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import RectangleSelector
+from matplotlib.backend_bases import MouseButton
+
+# Plot resolution, in pixels in x and y directions
+resolution = 400, 200
+
+# Centre and range of the plot, x and y
+centrex, centrey = 0, 0
+rangex, rangey = 4, 2
+
+# Initial max iterations for the Mandelbrot plotting algorithm.
+# The higher the more details in the plot, and slower to calculate.
+# This will be changed iteractivelly by Up- and Down-keys.
+current_max_iters = 99
+
+
+def progbar(x, xi, xf, size=60, label='Progress:'):
+    done = int(round(size * (x - xi) / (xf - xi)))
+    if done == size:
+        print(' ' * (len(label) + size + 5), end='\r')
+    else:
+        rest = size - done
+        print('%s |%s>%s|' % (label, '='*done, '-'*rest), end='\r')
+
+
+def onclick(event):
+    global plotted_domain, selected_domain
+
+    if event.button == 1 and event.dblclick:
+        update(selected_domain)
+
+    if event.button == 3:
+        x1, x2, y1, y2 = plotted_domain
+        selected_domain = np.array(
+                (x1 - 0.5, x2 + 0.5,
+                 y1 - 0.5, y2 + 0.5))
+        update(selected_domain)
+
+
+def line_select_callback(eclick, erelease):
+    'eclick and erelease are the press and release events'
+    x1, y1 = eclick.xdata, eclick.ydata
+    x2, y2 = erelease.xdata, erelease.ydata
+
+    if eclick.button == MouseButton.LEFT:
+        global selected_domain
+        selected_domain = np.array((x1, x2, y1, y2))
+        print('Selected: {:8.3g} {:8.3g} {:8.3g}'.format(*selected_domain), end='\r')
+
+
+def toggle_selector(event):
+    global current_max_iters, plotted_domain
+    if event.key == 'up':
+        current_max_iters *= 2
+        update(plotted_domain)
+    if event.key == 'down':
+        current_max_iters /= 10
+        update(plotted_domain)
+    if event.key in ['R', 'r']:
+        toggle_selector.RS.set_active(not toggle_selector.RS.active)
+        print('%sctivated rectangle selection.' % ('A' if toggle_selector.RS.active else 'Dea'))
+
+
+def mandel(domain=(-2.5, 1, -1, 1),  # x1, x2, y1, y2
+           resolution=(1920, 1080),
+           threshold=4,
+           max_iters=99,
+           ):
+    x = np.linspace(domain[0], domain[1], resolution[0])
+    y = np.linspace(domain[2], domain[3], resolution[1])
+
+    xy_count_iters = []
+    for y0 in y:
+        x_count_iters = []
+        for x0 in x:
+            x1, y1 = 0, 0
+            i = 0
+
+            while x1*x1 + y1*y1 <= threshold and i < max_iters:
+                xtmp = x1*x1 - y1*y1 + x0
+                y1 = 2*x1*y1 + y0
+                x1 = xtmp
+                i += 1
+            x_count_iters.append(i)
+        progbar(y0, *domain[2:4])
+        xy_count_iters.append(x_count_iters)
+
+    return np.array(xy_count_iters)
+
+
+def update(domain):
+    global plotted_domain
+    plotted_domain = domain
+    print('Max iters: {} Domain: {:8.3g} {:8.3g} {:8.3g}'.format(current_max_iters, *domain))
+    grid = mandel(resolution=resolution, domain=domain, max_iters=current_max_iters)
+    plt.imshow(grid, cmap=plt.cm.RdBu, interpolation='bilinear', extent=domain)
+    plt.draw()
+
+
+fig, ax = plt.subplots()
+toggle_selector.RS = RectangleSelector(
+        ax, line_select_callback,
+        drawtype='box', useblit=True,
+        button=[1],  # react to left button only
+        minspanx=5, minspany=5,
+        spancoords='pixels',
+        interactive=True)
+fig.canvas.mpl_connect('button_press_event', onclick)
+plt.connect('key_press_event', toggle_selector)
+
+selected_domain = np.array(
+        (centrex - rangex, centrex + rangex,
+         centrey - rangey, centrey + rangey))
+
+update(selected_domain)
+plt.show()
